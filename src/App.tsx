aba20b3857791719b1c6de165f21d8d2c98e5456
unknown <hong.gilseong@gmail.com>
2022-04-03 00:04:08 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import Board from "./components/Board";
 
 function App() {
   return (
     <div className="flex justify-center items-center w-full flex-col h-full ">
       <h1 className="text-2xl font-bold mb-10">지뢰찾기</h1>
-      <Board />
     </div>
   );
 }
