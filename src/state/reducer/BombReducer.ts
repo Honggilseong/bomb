290ec7ce48adbe4935fdc761732d479285ba85d9
unknown <hong.gilseong@gmail.com>
2022-04-03 18:24:01 +0900

    feature: create search near bombs by clicking

--- src/state/reducer/BombReducer.ts
+++ src/state/reducer/BombReducer.ts
@@ -41,7 +41,38 @@ const BombReducer = (
       const newTable = [...state.table];
 
       newTable[intRow] = [...state.table[intRow]];
-      newTable[intRow][intCell] = CODE.OPENED;
+      // 주변 지뢰 탐색
+      let around: number[] = [];
+      if (newTable[intRow - 1]) {
+        // 클릭한 칸 기준으로 윗줄이 있으면 아랫줄을 배열에 추가해준다.
+        around = around.concat(
+          newTable[intRow - 1][intCell - 1],
+          newTable[intRow - 1][intCell],
+          newTable[intRow - 1][intCell + 1]
+        );
+      }
+      console.log("1", around);
+      // 클릭한 칸 기준으로 앞 뒤를 배열에 추가해준다.
+      around = around.concat(
+        newTable[intRow][intCell - 1],
+        newTable[intRow][intCell + 1]
+      );
+      console.log("2", around);
+      // 클릭한 칸 기준으로 밑에줄이 있으면 아랫줄을 배열에 추가해준다.
+      if (newTable[intRow + 1]) {
+        around = around.concat(
+          newTable[intRow + 1][intCell - 1],
+          newTable[intRow + 1][intCell],
+          newTable[intRow + 1][intCell + 1]
+        );
+      }
+      console.log("3", around);
+      // 주변 지뢰 갯수 찾고 그 길이만큼 count에 값을추가하여
+      const count = around.filter((bomb) =>
+        [CODE.BOMB, CODE.FLAG_BOMB, CODE.QUESTION_BOMB].includes(bomb)
+      ).length;
+      // 클릭한 칸에 카운트 값을 추가해줍니다.
+      newTable[intRow][intCell] = count;
       return { ...state, table: newTable };
     }
     case CLICK_BOMB: {
